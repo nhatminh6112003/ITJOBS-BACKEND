7a7cad2c11586a0d26256ac3129f0c133b77a246
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-06-30 22:44:17 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "*",
+		origin: ['http://127.0.0.1:5173', 'http://localhost:3000'],
 		credentials: true,
 		optionsSuccessStatus: 200
 	})
