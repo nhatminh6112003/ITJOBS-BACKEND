37e6fd1ac657574b93ae31f3a0521fb9d604c6de
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-07-31 19:22:09 +0700

    fix/resume_title

--- src/services/resume_title.service.js
+++ src/services/resume_title.service.js
@@ -4,6 +4,16 @@ import { resumeStatusEnum } from '@src/constants/resumeStatus';
 import { findByPkAndUpdate } from '@src/helpers/databaseHelpers';
 
 const resumeTitleService = {
+	async getOne(resume_id) {
+		const findResume = await resume_title.findOne({
+			where: {
+				resume_id
+			},
+			raw: true
+		});
+		if (!findResume) throw createError(404, 'Không tìm thấy bản ghi');
+		return findResume;
+	},
 	async update(resume_id, data) {
 		return await findByPkAndUpdate(resume_title, resume_id, {
 			...data,
