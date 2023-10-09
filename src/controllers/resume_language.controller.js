495c5ae1c8dc9e82ccdba1ded9e45733dee61a0d
dat <MY_NAME@example.com>
2023-10-09 16:46:59 +0700

    Implement update feat/resume_language

--- src/controllers/resume_language.controller.js
+++ src/controllers/resume_language.controller.js
@@ -2,6 +2,11 @@ import asyncHandlerDecorator from '@src/helpers/asyncHandlerDecorator';
 import resumeLanguageService from '@src/services/resume_language.service';
 
 const resumeLanguageController = {
+	async getAll(req, res) {
+		const { id } = req.params;
+		const data = await resumeLanguageController.getAllByResume(id);
+		return res.apiResponse(data);
+	},
 	async create(req, res) {
 		const data = req.body;
 		const handleCreate = await resumeLanguageService.create(data);
