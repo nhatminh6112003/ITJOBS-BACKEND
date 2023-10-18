57b1fcc6cb537e399235c034a6d912eb9ada25d5
dat <MY_NAME@example.com>
2023-10-18 21:06:52 +0700

    Implement job_profession_detail feature

---
+++ src/routes/job_profession_detail.route.js
@@ -0,0 +1,11 @@
+import job_profession_detailController from '@src/controllers/job_profession_detail.controller';
+import Express from 'express';
+
+const router = Express.Router();
+router.get('/getAll/:id',job_profession_detailController.getAll);
+router.get('/:id',job_profession_detailController.getOne);
+router.post('',job_profession_detailController.create);
+router.patch('/:id',job_profession_detailController.update);
+router.delete('/:id',job_profession_detailController.delete);
+
+export default router;
