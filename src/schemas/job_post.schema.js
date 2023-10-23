95b7f1ca3e2e0b3c12f921ca47e06367031dbed8
Minh Trường <letruong147123@gmail.com>
2023-10-23 09:53:43 +0700

    fix model job_post

--- src/schemas/job_post.schema.js
+++ src/schemas/job_post.schema.js
@@ -2,9 +2,9 @@ import Joi from 'joi';
 
 const jobPostSchema = {
 	jobPost: Joi.object({
-		job_welfare_id:Joi.array().items(Joi.number()),
-		job_work_type_id:Joi.array().items(Joi.number()),
-		job_profession_id:Joi.array().items(Joi.number()),
+		job_welfare_id: Joi.array().items(Joi.number()),
+		job_work_type_id: Joi.array().items(Joi.number()),
+		job_profession_id: Joi.array().items(Joi.number()),
 		posted_by_id: Joi.string().required(),
 		company_id: Joi.string().required(),
 		job_degree_value: Joi.number().required(),
@@ -14,18 +14,20 @@ const jobPostSchema = {
 		form_age: Joi.string(),
 		to_age: Joi.string(),
 		job_title: Joi.string(),
-		gender: Joi.string().valid('0', '1', '2'),
+		gender: Joi.number().valid(0, 1, 2),
 		is_address_work_hidden: Joi.boolean(),
 		min_salary: Joi.number().required(),
 		max_salary: Joi.number().required(),
 		posted_date: Joi.date(),
 		job_desc: Joi.string(),
 		job_request: Joi.string().required(),
-		status: Joi.string().valid('0', '1', '2'),
+		status: Joi.number().valid(0, 1, 2, 3),
 		work_home: Joi.boolean(),
 		isDeleted: Joi.boolean(),
 		job_formExperience: Joi.number(),
-		job_ToExperience: Joi.number()
+		job_ToExperience: Joi.number(),
+		provinces: Joi.number().required(),
+		districts: Joi.number().required()
 	})
 };
 
