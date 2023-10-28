291a5031234c35774a1cf5e22790cb6fef60b9ac
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-10-28 17:11:58 +0700

    company edit

--- src/helpers/uploadMulter.js
+++ src/helpers/uploadMulter.js
@@ -8,7 +8,6 @@ const storage = multer.diskStorage({
 		cb(null, uploadDir);
 	},
 	filename: (req, file, cb) => {
-		console.log("TCL: file", file)
 		const uniqueSuffix = `${Date.now()  }-${  Math.round(Math.random() * 1e9)}`;
 		cb(null, `${file.originalname}_${uniqueSuffix}.${file.mimetype.split('/')[1]}`);
 	}
--- src/routes/company.route.js
+++ src/routes/company.route.js
@@ -1,12 +1,13 @@
 import companyController from '@src/controllers/company.controller';
 import Express from 'express';
+import uploadMulter from '@src/helpers/uploadMulter';
 
 const router = Express.Router();
 
 router.post('', companyController.create);
 router.get('', companyController.getAll);
 router.get('/:id', companyController.getOne);
-router.patch('/:id',companyController.update);
+router.patch('/:id', uploadMulter.array('files'), companyController.update);
 router.delete('/:id', companyController.delete);
 
 export default router;
