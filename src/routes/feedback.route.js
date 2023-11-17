e2e89163e60c4899fc0eefa6407c208c53225bac
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-11-17 08:28:18 +0700

    feat: feedback

---
+++ src/routes/feedback.route.js
@@ -0,0 +1,21 @@
+import feedbackController from '@src/controllers/feedback.controller';
+
+import Express from 'express';
+
+const router = Express.Router();
+router.get('', feedbackController.getAll);
+router.get('/:id', feedbackController.getOne);
+router.post(
+	'',
+	feedbackController.create
+);
+router.patch(
+	'/:id',
+	feedbackController.update
+);
+router.delete(
+	'/:id',
+	feedbackController.delete
+);
+
+export default router;
