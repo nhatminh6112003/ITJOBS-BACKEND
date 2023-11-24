b8cde2aae727058c6c2ab6ee394a94db524542b4
IceTea <iceteawebdev@gmail.com>
2023-11-25 01:29:11 +0700

    company-service

--- src/services/company_service.service.js
+++ src/services/company_service.service.js
@@ -1,29 +1,47 @@
 import dotenv from 'dotenv';
-import createError from "http-errors";
+import createError from 'http-errors';
 import { company_service } from '../models';
-import { findByPkAndUpdate, findByPkAndDelete} from '../helpers/databaseHelpers';
+import { findByPkAndUpdate, findByPkAndDelete } from '../helpers/databaseHelpers';
+import moment from 'moment/moment';
 
 dotenv.config();
 const companyServiceService = {
-    async getAll() {
+	async getAll() {
 		return await company_service.findAll();
 	},
-    
-    async getOne(id) {
-		const dataOne= await company_service.findOne({
-            where:{
-                id
-            },
-            raw: true
-        });
-        if(!dataOne){
-            throw createError(404,"Không tìm thấy thông tin công ty")
-        }
-        return dataOne
+
+	async getOne(id) {
+		const dataOne = await company_service.findOne({
+			where: {
+				id
+			},
+			raw: true
+		});
+		if (!dataOne) {
+			throw createError(404, 'Không tìm thấy thông tin công ty');
+		}
+		return dataOne;
 	},
 
 	async create(data) {
-		return await company_service.create(data);
+		const date = new Date();
+		const { user_account_id, company_id, service_id } = data;
+		const dataOne = await company_service.findOne({
+			where: {
+				user_account_id,
+				company_id,
+				service_id
+			},
+			raw: true
+		});
+		if (!dataOne) {
+			const register_date = moment(date).format('YYYYMMDD');
+			const expiration_date = moment(register_date, 'YYYYMMDD').add(30, 'days').format('YYYYMMDD');
+			return await company_service.create({ user_account_id, company_id, service_id, register_date, expiration_date });
+		}
+		const { expiration_date, id } = dataOne;
+		const newExiration_date = moment(expiration_date, 'YYYYMMDD').add(30, 'days').format('YYYYMMDD');
+		return await findByPkAndUpdate(company_service, id, { expiration_date: newExiration_date });
 	},
 
 	async update(id, data) {
