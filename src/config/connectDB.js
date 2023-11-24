9f46acd31df0195c34473b6f9b4328f1c18a353f
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-11-24 22:16:16 +0700

    fix: database connect

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,8 +2,17 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-	host: 'localhost',
+const DB_NAME = process.env.NODE_ENV === 'production' ? process.env.DB_PRODUCTION_NAME : process.env.DB_NAME;
+
+const USER_NAME =
+	process.env.NODE_ENV === 'production' ? process.env.DB_PRODUCTION_DB_USERNAME : process.env.DB_USERNAME;
+
+const DB_PASSWORD =
+	process.env.NODE_ENV === 'production' ? process.env.DB_PRODUCTION_PASSWORD : process.env.DB_PASSWORD;
+
+const DB_HOST = process.env.NODE_ENV === 'production' ? process.env.DB_PRODUCTION_HOST : process.env.DB_HOST;
+export const sequelize = new Sequelize(DB_NAME, USER_NAME, DB_PASSWORD, {
+	host: DB_HOST,
 	logging: true,
 	pool: {
 		max: 5,
@@ -11,7 +20,9 @@ export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERN
 		acquire: 30000,
 		idle: 10000
 	},
-	dialect: 'mysql'
+	dialect: 'mysql',
+	// eslint-disable-next-line global-require
+	dialectModule: require('mysql2')
 });
 export const connectDb = async () => {
 	try {
