085b8fc2971aaea1c603c8565106d2b98e7310e3
Minh Trường <letruong147123@gmail.com>
2023-12-04 21:25:20 +0700

    fix: analyticJobPost

--- src/routes/profession.route.js
+++ src/routes/profession.route.js
@@ -2,32 +2,12 @@ import Express from 'express';
 import professionController from '../controllers/profession.controller';
 import ProfessionValidation from '../validations/profession.validation';
 
-import UserRoleEnum from '../constants/userRoles';
-import AuthMiddleWare from '../middleware/authMiddleware';
-
 const router = Express.Router();
 router.get('', professionController.getAll);
 router.get('/analysisProfession', professionController.analysisProfession);
 router.get('/:id', professionController.getOne);
-router.post(
-	'',
-	AuthMiddleWare.protect,
-	ProfessionValidation.profession,
-	AuthMiddleWare.authPage([UserRoleEnum.ADMIN]),
-	professionController.create
-);
-router.patch(
-	'/:id',
-	AuthMiddleWare.protect,
-	ProfessionValidation.profession,
-	AuthMiddleWare.authPage([UserRoleEnum.ADMIN]),
-	professionController.update
-);
-router.delete(
-	'/:id',
-	AuthMiddleWare.protect,
-	AuthMiddleWare.authPage([UserRoleEnum.ADMIN]),
-	professionController.delete
-);
+router.post('', ProfessionValidation.profession, professionController.create);
+router.patch('/:id', ProfessionValidation.profession, professionController.update);
+router.delete('/:id', professionController.delete);
 
 export default router;
