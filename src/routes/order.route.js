29ab6ca1815609eb7dc2ed268e4a41eaea6e33b2
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-12-05 10:37:17 +0700

    fix: order

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,12 +1,11 @@
 import Express from 'express';
 import orderController from '../controllers/order.controller';
-import AuthMiddleWare from '../middleware/authMiddleware';
 
 const router = Express.Router();
 router.get('', orderController.getAll);
 router.get('/:id', orderController.getOne);
-router.post('', AuthMiddleWare.protect, orderController.create);
-router.patch('/:id', AuthMiddleWare.protect, orderController.update);
-router.delete('/:id', AuthMiddleWare.protect, orderController.delete);
+router.post('', orderController.create);
+router.patch('/:id', orderController.update);
+router.delete('/:id', orderController.delete);
 
 export default router;
